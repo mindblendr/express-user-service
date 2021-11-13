b1eeeef82d12730585b50b6e7ff572d1b91c6157
Riclacuey02 <ricardoiib.lacuesta@gmail.com>
2021-11-13 14:08:12 +0800

    service.js get route sample

--- server.js
+++ server.js
@@ -11,6 +11,11 @@ app.use(cors());
 app.use(express.json());
 initRoutes(app);
 
+app.get('/', (req, res) => {
+    res.send('Hello World!!!!');
+    // res.sendFile(__dirname + '/public/index.html');
+});
+
 server.listen(port, async () => {
     console.log(`Listening on port ${port}!`);
 });
